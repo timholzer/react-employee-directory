14462c1fcb6e221fced1b02b0fde8fe420e85a06
Tim Holzer <timiholzer@gmail.com>
2021-02-04 22:47:05 -0700

    set up folders and api.js for getting employees

---
+++ src/utils/api.js
@@ -0,0 +1,8 @@
+
+import axios from "axios";
+
+export default {
+    getEmployees: count => {
+        return axios.get(`https://randomuser.me/api/?results=${count}&inc=name,email&nat=us`);
+    }
+}
